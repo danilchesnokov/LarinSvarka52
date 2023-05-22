389244abcb6f7809d3d280b9a29936ed2df9b433
tannshay <shaytann831@gmail.com>
2023-05-22 15:01:39 +0300

    start commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,17 @@
+export default function Navigation(){
+    return(
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Калькулятор</a>
+                </li>
+                <li>
+                    <a href="#">Портфолио</a>
+                </li>
+                <li>
+                    <a href="#">Контакты</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
---
+++ src/components/WhyUs.js
@@ -0,0 +1,5 @@
+export default function WhyUs(){
+    return(
+        <h1>Почему мы?</h1>
+    )
+}
